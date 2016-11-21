3ae6970e31bc783a454666fe4409a9dc07637815
DESKTOP-FQCLTKM\Popov <popovfilip444@gmail.com>
2016-11-22 00:07:16 +0200

    index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$uri = $_SERVER['REQUEST_URI'];
+$self = $_SERVER['PHP_SELF'];
+
+$self = str_replace('index.php', '', $self);
+
+$uri = str_replace($self, '', $uri);
+
+$args = explode('/', $uri);
+$controllerName = ucfirst(array_shift($args)) . 'Controller';
+$actionName = array_shift($args);
+
+
+if (class_exists($controllerName)) {
+    $controller = new $controllerName();
+    call_user_func_array(
+        [
+            $controller,
+            $actionName
+        ],
+        $args
+    );
+} else {
+    echo "<h1>Page Not Found</h1>";
+}
